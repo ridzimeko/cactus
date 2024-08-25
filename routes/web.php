2209aae80ba1676f2f10e389e48b47c18607cf3b
Amro Alfien <ridzimeko@proton.me>
2024-08-25 21:06:09 +0700

    remove ask route

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,8 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('/user/{username}', 'pages.profile.view')->name('profile.view');
     Volt::route('/profile/edit', 'pages.profile.edit')->name('profile.edit');
     Volt::route('/notification', 'pages.notification')->name('notification');
-    Volt::route('/ask', 'pages.ask')->name('ask');
     Volt::route('/search', 'pages.search')->name('search');
     Volt::route('/question/{question_id}', 'pages.question-detail')->name('question.view');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
