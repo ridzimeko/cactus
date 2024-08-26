3e310755f2d78ecf4884e547fc334504f8552819
Amro Alfien <ridzimeko@proton.me>
2024-08-26 19:37:28 +0700

    feat(navbar): account menu

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -6,11 +6,11 @@ use Livewire\Volt\Component;
 
 new class extends Component {
     public $class;
-    public $username;
+    public $user;
 
     public function mount()
     {
-        $this->username = Auth::user()->username;
+        $this->user = Auth::user();
     }
 
     /**
@@ -34,16 +34,39 @@ new class extends Component {
             </div>
         </x-cactus-logo>
         <div class="flex flex-col gap-2">
-            <x-nav-link :active="request()->routeIs('home')" icon="heroicon-o-home" active_icon="heroicon-m-home" :href="route('home')" wire:navigate>Beranda</x-nav-link>
-            <x-nav-link :active="request()->routeIs('search')" icon="heroicon-o-magnifying-glass" active_icon="heroicon-m-magnifying-glass" :href="route('search')"
-                wire:navigate>Cari</x-nav-link>
+            <x-nav-link :active="request()->routeIs('home')" icon="heroicon-o-home" active_icon="heroicon-m-home" :href="route('home')"
+                wire:navigate>Beranda</x-nav-link>
+            <x-nav-link :active="request()->routeIs('search')" icon="heroicon-o-magnifying-glass" active_icon="heroicon-m-magnifying-glass"
+                :href="route('search')" wire:navigate>Cari</x-nav-link>
 
-            <x-nav-link :active="request()->routeIs('notification')" icon="heroicon-o-bell" active_icon="heroicon-s-bell" :href="route('notification')" wire:navigate>Notifikasi</x-nav-link>
-            <x-nav-link :active="request()->path() === 'user/' . $username" icon="heroicon-o-user" active_icon="heroicon-s-user" :href="route('profile.view', [$username])" wire:navigate>Profil</x-nav-link>
-            <x-primary-button x-on:click="showPostForm = true" class="mt-6 flex gap-4 items-center" type="button">
+            <x-nav-link :active="request()->routeIs('notification')" icon="heroicon-o-bell" active_icon="heroicon-s-bell" :href="route('notification')"
+                wire:navigate>Notifikasi</x-nav-link>
+            <x-nav-link :active="request()->path() === 'user/' . $user->username" icon="heroicon-o-user" active_icon="heroicon-s-user" :href="route('profile.view', [$user->username])"
+                wire:navigate>Profil</x-nav-link>
+            <x-primary-button x-on:click="showPostForm = true" class="mt-6 flex items-center gap-4" type="button">
                 @svg('heroicon-o-pencil-square', 'size-8')
                 <span class="hidden lg:block">Tanya</span>
             </x-primary-button>
+        </div>
+    </div>
+
+    <div x-data="{ 'showAccountMenu': false }">
+        <div x-on:click="showAccountMenu = !showAccountMenu" x-on:click.outside="showAccountMenu = false"
+            class="flex h-14 w-auto cursor-pointer select-none items-center justify-center rounded-full transition lg:hover:bg-gray-800"
+            x-bind:class="showAccountMenu ? 'lg:bg-gray-800' : ''">
+            <x-user-avatar :avatar="$user->profile_img" size="small" />
+            <div class="ml-3 hidden flex-1 lg:block">
+                <p class="overflow-hidden text-ellipsis text-base">{{ $user->name }}</p>
+                <p class="overflow-hidden text-ellipsis text-base text-gray-500">{{ '@' . $user->username }}</p>
+            </div>
+        </div>
+
+        <div x-show="showAccountMenu"
+            class="fixed bottom-24 z-40 w-[200px] rounded-lg bg-gray-800/90 transition-opacity hover:bg-gray-700">
+            <button wire:click="logout" class="flex w-full gap-2 px-4 py-2">
+                <x-heroicon-o-arrow-left-on-rectangle class="size-6" />
+                <span>Keluar</span>
+            </button>
         </div>
     </div>
 </nav>
