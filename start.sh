ef0df18d3926c9c1eebff552aadd0612db8c807a
Amro Alfien <ridzimeko@proton.me>
2025-03-11 13:11:55 +0700

    chore: setup nginx

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
+echo "generating application key..."
+php artisan key:generate --show
+
 echo "Caching config..."
 php artisan config:cache
 
