6c8e0f9093203e8eb9153fdffecd449dae6a2e1c
Amro Alfien <ridzimeko@proton.me>
2025-03-11 11:39:22 +0700

    vercel config init

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->create()
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    });
