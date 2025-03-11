2b196d6d6e9a79bcc015ce25eddd327837809ea8
Amro Alfien <ridzimeko@proton.me>
2025-03-11 11:54:52 +0700

    fix app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create()
+    })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
-    });
+    })->create();
